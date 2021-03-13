9533954622f2297ca9aa38ac011cc8a0e2b031ed
endezz <r-serega13@yandex.ru>
2021-03-13 22:48:39 +0400

    feat: complite typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < array[0]) {
+            array[0] = array[i];
+        }
+    }
+    return array[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) return 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > array[0]) {
+            array[0] = array[i];
+        }
+    }
+    return array[0];
+};
+
+exports.avg = function avg(array) {
+    let res = 0;
+
+    if (!array || array.length === 0) return 0;
+
+    for (let i = 0; i < array.length; i++) {
+        res += array[i];
+    }
+    return res / array.length;
+};
